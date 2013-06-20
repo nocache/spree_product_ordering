54343620ff4180c6e679ab66dc573cdf010f63b0
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2013-06-20 11:35:22 +0800

    Use table name from Spree::Product AR model class

--- db/migrate/20090908001137_add_position_to_products.rb
+++ db/migrate/20090908001137_add_position_to_products.rb
@@ -1,9 +1,9 @@
 class AddPositionToProducts < ActiveRecord::Migration
   def self.up
-    add_column :products, :position, :integer
+    add_column Spree::Products.table_name, :position, :integer
   end
 
   def self.down
-    remove_column :products, :position
+    remove_column Spree::Products.table_name, :position
   end
 end
